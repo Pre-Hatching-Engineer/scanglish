af2dae63e357ce97713c5e33f20fdecc24ea0953
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2024-12-01 22:18:44 +0900

    apiのCORSの設定

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,27 @@
 package main
 
-import "github.com/labstack/echo/v4"
+import (
+	"os"
+	"strings"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
 func main() {
 	e := echo.New()
+	e.HideBanner = true
+	e.HidePort = true
+	e.Use(middleware.Recover())
+	e.Use(middleware.Secure())
 
-	e.GET("")
+	// cors
+	origins := []string{"*"}
+	if os.Getenv("ALLOW_ORIGINS") != "" {
+		origins = strings.Split(os.Getenv("ALLOW_ORIGINS"), ",")
+	}
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: origins,
+	}))
+
 }
