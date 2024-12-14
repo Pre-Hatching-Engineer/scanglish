f2beea30db358c23108b8034229f16d668d6e952
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2024-12-14 22:56:03 +0900

    大枠の実装

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"context"
+	"net/http"
 	"os"
+	"os/signal"
 	"strings"
+	"time"
+
+	appmiddle "github.com/Pre-Hatching-Engineer/scanglish/internal/app/middleware"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
 )
 
 func main() {
 	e := echo.New()
 	e.HideBanner = true
 	e.HidePort = true
+	e.Logger.SetLevel(log.Info())
 	e.Use(middleware.Recover())
 	e.Use(middleware.Secure())
 
@@ -24,4 +32,22 @@ func main() {
 		AllowOrigins: origins,
 	}))
 
+	// auth
+	e.Use(appmiddle.AuthMiddleware())
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	go func() {
+		if err := e.Start(":3001"); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	<-ctx.Done()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
